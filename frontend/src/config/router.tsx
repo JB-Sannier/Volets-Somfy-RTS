0f580aea6de01c51b2493844dd53a4aee095c4d1
ichigirl <sichigo@furry-femboys.com>
2026-03-04 21:55:49 +0100

    feat: enhance SEO and accessibility with Open Graph and Twitter meta tags in index.html feat: add ErrorBoundary component for better error handling in the application feat: implement ErrorPage for displaying error messages to users refactor: update router configuration to include error handling for routes chore: add i18next for internationalization support in the application

--- frontend/src/config/router.tsx
+++ frontend/src/config/router.tsx
@@ -1,16 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
-import { WelcomePage } from "../pages/welcome-page";
-import { LoginPage } from "../pages/login-page";
-import { ShuttersManagementPage } from "../pages/shutters-management-page";
 import { AddShutterPage } from "../pages/add-shutter-page";
-import { UsersManagementPage } from "../pages/users-management-page";
 import { AddUserPage } from "../pages/add-user-page";
+import { ErrorPage } from "../pages/error-page";
+import { LoginPage } from "../pages/login-page";
 import { ModifyUserPage } from "../pages/modify-user-page";
+import { ShuttersManagementPage } from "../pages/shutters-management-page";
+import { UsersManagementPage } from "../pages/users-management-page";
+import { WelcomePage } from "../pages/welcome-page";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: WelcomePage,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/login",
@@ -31,9 +33,15 @@ export const router = createBrowserRouter([
   {
     path: "/users-management/modify-user/:email",
     element: <ModifyUserPage />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/users-management/add-user",
     Component: AddUserPage,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "*",
+    element: <ErrorPage />,
   },
 ]);
