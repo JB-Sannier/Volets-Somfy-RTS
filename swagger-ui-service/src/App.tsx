da73c9a2fd2156b7d10492e1adf87e8c34ab088e
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2026-05-08 15:55:58 +0200

    chore: biome lint and format on swagger-ui-service

--- swagger-ui-service/src/App.tsx
+++ swagger-ui-service/src/App.tsx
@@ -3,11 +3,7 @@ import SwaggerUi from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
 function App() {
-  return (
-    <>
-      <SwaggerUi url="/swagger.yml" />
-    </>
-  );
+	return <SwaggerUi url="/swagger.yml" />;
 }
 
 export default App;
