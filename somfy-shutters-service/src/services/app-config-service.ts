7af39f3787a81eebf70a7393116e7ed22e8138d5
JB-Sannier <>
2026-07-12 19:46:40 +0200

    chore: somfy-shutters-service: formatting issue

--- somfy-shutters-service/src/services/app-config-service.ts
+++ somfy-shutters-service/src/services/app-config-service.ts
@@ -5,76 +5,76 @@ import * as dotenv from "dotenv";
 export const appConfigServiceKey = "AppConfigService";
 
 export interface IAppConfigService {
-	environment(): string;
-	port(): number;
-	host(): string;
-	somfyServerHostName(): string;
-	somfyServerPort(): number;
-	somfyPassword(): string;
-	selfApiKey(): string;
-	dbHostName(): string;
-	dbPort(): number;
-	dbUserName(): string;
-	dbPassword(): string;
-	dbName(): string;
-	dbSchema(): string;
+  environment(): string;
+  port(): number;
+  host(): string;
+  somfyServerHostName(): string;
+  somfyServerPort(): number;
+  somfyPassword(): string;
+  selfApiKey(): string;
+  dbHostName(): string;
+  dbPort(): number;
+  dbUserName(): string;
+  dbPassword(): string;
+  dbName(): string;
+  dbSchema(): string;
 }
 
 @provide(appConfigServiceKey)
 export class AppConfigServiceFromEnv implements IAppConfigService {
-	constructor() {
-		dotenv.config({ quiet: true });
-	}
+  constructor() {
+    dotenv.config({ quiet: true });
+  }
 
-	environment(): string {
-		return process.env.ENV || "dev";
-	}
+  environment(): string {
+    return process.env.ENV || "dev";
+  }
 
-	host(): string {
-		return process.env.HOSTNAME || "127.0.0.1";
-	}
+  host(): string {
+    return process.env.HOSTNAME || "127.0.0.1";
+  }
 
-	port(): number {
-		return parseInt(process.env.PORT || "3001", 10);
-	}
+  port(): number {
+    return parseInt(process.env.PORT || "3001", 10);
+  }
 
-	somfyServerHostName(): string {
-		return process.env.SOMFY_SERVER_HOST_NAME || "127.0.0.1";
-	}
+  somfyServerHostName(): string {
+    return process.env.SOMFY_SERVER_HOST_NAME || "127.0.0.1";
+  }
 
-	somfyServerPort(): number {
-		return parseInt(process.env.SOMFY_SERVER_PORT || "3000", 10);
-	}
+  somfyServerPort(): number {
+    return parseInt(process.env.SOMFY_SERVER_PORT || "3000", 10);
+  }
 
-	somfyPassword(): string {
-		return process.env.SOMFY_SERVER_PASSWORD || "";
-	}
+  somfyPassword(): string {
+    return process.env.SOMFY_SERVER_PASSWORD || "";
+  }
 
-	selfApiKey(): string {
-		return process.env.SELF_API_KEY || "not-defined";
-	}
+  selfApiKey(): string {
+    return process.env.SELF_API_KEY || "not-defined";
+  }
 
-	dbHostName(): string {
-		return process.env.DB_HOSTNAME || "";
-	}
+  dbHostName(): string {
+    return process.env.DB_HOSTNAME || "";
+  }
 
-	dbPort(): number {
-		return parseInt(process.env.DB_PORT || "5432", 10);
-	}
+  dbPort(): number {
+    return parseInt(process.env.DB_PORT || "5432", 10);
+  }
 
-	dbUserName(): string {
-		return process.env.DB_USERNAME || "";
-	}
+  dbUserName(): string {
+    return process.env.DB_USERNAME || "";
+  }
 
-	dbPassword(): string {
-		return process.env.DB_PASSWORD || "";
-	}
+  dbPassword(): string {
+    return process.env.DB_PASSWORD || "";
+  }
 
-	dbName(): string {
-		return process.env.DB_NAME || "";
-	}
+  dbName(): string {
+    return process.env.DB_NAME || "";
+  }
 
-	dbSchema(): string {
-		return process.env.DB_SCHEMA || "";
-	}
+  dbSchema(): string {
+    return process.env.DB_SCHEMA || "";
+  }
 }
